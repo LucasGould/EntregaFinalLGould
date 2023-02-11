672b82c5e5f791928a636f10a8d9802ef21c1512
Lucas Gould <lucas.gouldd@gmail.com>
2023-02-11 16:55:06 -0300

    borrador 0.1

--- JS/script.js
+++ JS/script.js
@@ -1,89 +1,66 @@
-let bebida;
-let cantidad;
-
-const fernet = 800
-const gin = 750
-const ron = 800
-const vermu = 645 
-
-
 function edad (){
-    let años = prompt('Ingrese su edad:')
-    if (años>17) {
+    let anios = prompt('Ingrese su edad:')
+    if (anios>17) {
         alert('Bienvenido a Drunk Sloth')
     } else {
     alert ('Acceso no permitido.')
 }} 
 
-function menu () {
-    bebida = parseFloat(prompt('Hacé tu pedido: \n 1: Fernet Branca con Coca-Cola \n 2: Gin Bombay con Agua Tónica Sweeps \n 3: Ron Bacardi con Coca-Cola y Limón \n 4: Vermouth Artesanal con Soda'))
-    switch (bebida) {
-        case 1:
-            cantidad = parseFloat(prompt('Seleccionaste Fernet Branca con Coca-Cola.\n Ingresá la cantidad'))
-            break
-        case 2:
-            cantidad = parseFloat(prompt('Seleccionaste Gin Bombay con Agua Tónica Sweeps.\n Ingresá la cantidad'))
-            break
-        case 3:
-            cantidad = parseFloat(prompt('Seleccionaste Ron Bacardi con Coca-Cola y Limón.\n Ingresá la cantidad'))
-            break
-        case 4:
-            cantidad = parseFloat(prompt('Seleccionaste Vermouth Artesanal con Soda.\n Ingresá la cantidad'))
-            break
-        default:
-            alert('Ingrese una opción válida.') 
-    }
+class Bebidas {
+    constructor(id, nombre, precio, descripcion, cantidad) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio
+    this.descripcion = descripcion
+    this.cantidad = cantidad
+}}
 
-    return {
-        bebidaElegida: bebida,
-        cantidadElegida: cantidad
-    }
-        
-    // pregunta = prompt('¿Desea seguir comprando? (Si/No)')
-    // if (pregunta == 'si') {
-    //     menu()
-    // } else if (pregunta == 'no') {
-    //     carrito()
-    // } else {
-    //     alert('Ingrese una opción válida.')
-    //     let pregunta = prompt('¿Desea seguir comprando? (Si/No)')
-    // }
+const lista = [
+    {id: 1, nombre: "Fernet con Coca", precio: 900, descripcion: 'Fernet Branca con Coca-Cola'},
+    {id: 2, nombre: "Gin-Tónic", precio: 850, descripcion: 'Gin Bombay con Agua Tónica Sweeps'},
+    {id: 3, nombre: "Cuba Libre", precio: 950, descripcion: 'Ron Bacardi con Coca-Cola'},
+    {id: 4, nombre: "Vermú Clásico", precio: 600, descripcion: 'Vermouth Artesanal con Soda'},
+]
+
+function opciones () {
+    let id = parseFloat(prompt('Ingrese la bebida deseada: \n 1: Fernet Branca con Coca-Cola \n 2: Gin Bombay con Agua Tónica Sweeps \n 3: Ron Bacardi con Coca-Cola y Limón \n 4: Vermouth Artesanal con Soda'));
+    let cantidad = parseFloat(prompt('Ingrese la cantidad deseada'));
+    const pedido = new pedidoFinal (id, nombre, precio, descripcion, cantidad);
+    carrito.push(pedidoFinal);
+    alert('Se añadió exitosamente')
 }
 
-function carrito (bebida, cantidadElegida) {
-    if (bebida === 1) {
-        alert('Se realizará la compra de Fernet Branca con Coca-Cola por $' + fernet * 1.12 *  cantidadElegida  + '\n (Precio final.)')
-    } else if (bebida === 2) {
-        alert('Se realizará la compra de Gin Bombay con Agua Tónica Sweeps por $' + gin * 1.12 *  cantidadElegida  + '\n (Precio final.)')
-    } else if (bebida === 3) {
-        alert('Se realizará la compra de Ron Bacardi con Coca-Cola y Limón por $' + ron * 1.12 *  cantidadElegida  + '\n (Precio final.)')
-    } else if (bebida === 4) {
-        alert('Se realizará la compra de Vermouth Artesanal con Soda por $' + vermu * 1.12 *  cantidadElegida  + '\n (Precio final.)')
-    } else {
-        menu()
-    }
+function verCarrito () {
+    carrito.forEach((producto) =>
+    alert(`Se han seleccionado ${producto.cantidad} ${producto.nombre} por un precio de ${producto.precio * producto.cantidad}`))
 }
 
-function envio () {
+let carrito = [];
+
+function envio (){
     prompt('Ingrese una dirección para realizar el envío (ejemplo: Calle Falsa 123)')
     alert('Gracias por su compra!')
 }
 
+let menu = parseFloat(prompt('Ingrese la opción deseada:\n 1. Comprar bebidas. \n 2. Ver carrito \n 3. Finalizar compra \n 4. Salir '))
+
+
 
 //          PROGRAMA
-edad()
+edad(menu)
 
-let proceso;
-do {
-    const {bebidaElegida, cantidadElegida} = menu()
-    let pregunta = prompt('¿Desea seguir comprando? (Si/No)')
-        if (pregunta === 'si') {
-            //Cómo puedo almacenar datos? 
-            //const {bebidaElegida, cantidadElegida} = menu()
-            //let pregunta = prompt('¿Desea seguir comprando? (Si/No)')   
-        } else {
-            carrito(bebidaElegida, cantidadElegida);
-            envio();
-            proceso = 'fin'
+while (menu !== 4) {
+    if (menu === 1){
+        opciones(carrito);
     }
-} while (proceso != 'fin')
+    if (menu == 2) {
+        verCarrito();
+    }
+    if (menu == 3){
+        finalizarCompra()
+    }
+    else {
+        alert('Ingrese una opción válida.')
+    }
+    let menu = parseFloat(prompt('Vuelva a ingresar una opción:\n 1. Comprar bebidas. \n 2. Ver carrito \n 3. Finalizar compra \n 4. Salir '))
+}
